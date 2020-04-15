84953ec2543996c99ede7649fe6e52be91737018
y0n1 <y0n1@outlook.com>
2020-04-15 16:33:43 +0300

    fix: Fixes an issue in filterImageFromURL
    - `UnhandledPromiseRejectionWarning: Error: Could not find MIME for Buffer <null>` when the image_url is invalid

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import Jimp = require('jimp');
+import fs from "fs";
+import Jimp from "jimp";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -8,18 +8,17 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
-    });
+export async function filterImageFromURL(inputURL: string) {
+  const photo = await Jimp.read(inputURL);
+  const outPath = `${__dirname}/tmp/filtered.${Math.floor(
+    Math.random() * 2000
+  )}.jpg`;
+  const processedImage = await photo
+    .resize(256, 256) // resize
+    .quality(60) // set JPEG quality
+    .greyscale() // set greyscale
+    .writeAsync(outPath);
+  return outPath;
 }
 
 // deleteLocalFiles
@@ -27,8 +26,8 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
 }
