4bb913bee3aac47601d5a3f32b5570c13d33c591
y0n1 <y0n1@outlook.com>
2020-04-15 10:38:52 +0300

    refactor: Implements the endpoint

--- src/server.ts
+++ src/server.ts
@@ -15,23 +15,34 @@ export default class Server {
 
     // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
     // GET /filteredimage?image_url={{URL}}
-    // app.get('/filteredimage', (res: Response, req: Request) => {
-    //   res.sendFile()
-    // });
-    // endpoint to filter an image from a public url.
-    // IT SHOULD
-    //    1. validate the image_url query
-    //    2. call filterImageFromURL(image_url) to filter the image
-    //    3. send the resulting file in the response
-    //    4. deletes any files on the server on finish of the response
-    // QUERY PARAMATERS
-    //    image_url: URL of a publicly accessible image
-    // RETURNS
-    //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+    /** endpoint to filter an image from a public url.
+     * IT SHOULD
+     *    1. validate the image_url query
+     *    2. call filterImageFromURL(image_url) to filter the image
+     *    3. send the resulting file in the response
+     *    4. deletes any files on the server on finish of the response
+     * QUERY PARAMATERS
+     *    image_url: URL of a publicly accessible image
+     * RETURNS
+     *   the filtered image file
+     */
+    app.get("/filteredimage", async (req, res) => {
+      const { image_url } = req.query;
+      if (!image_url) {
+        return res.sendStatus(400);
+      }
 
-    /**************************************************************************** */
+      let url: URL;
+      try {
+        url = new URL(image_url);
+      } catch {
+        return res.status(400).send("Invalid URL");
+      }
 
-    //! END @TODO1
+      const filteredPath = await filterImageFromURL(image_url);
+      res.sendFile(filteredPath);
+      deleteLocalFiles([filteredPath])
+    });
 
     // Root Endpoint
     // Displays a simple message to the user
